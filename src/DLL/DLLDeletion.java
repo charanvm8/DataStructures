3b5e544c9ab3868be45a53f56ddf563c416b09dd
charanvm8@gmail.com <charanvm8@gmail.com>
2019-03-20 22:44:19 -0400

    new changes

--- src/DLL/DLLDeletion.java
+++ src/DLL/DLLDeletion.java
@@ -1,4 +1,13 @@
 package DLL;
 
 public class DLLDeletion {
+    Node head;
+
+    public void deleteAtFirst(){
+
+    }
+
+    public void deleteAtLast(){
+
+    }
 }
--- src/DLL/DLLInsertion.java
+++ src/DLL/DLLInsertion.java
@@ -4,10 +4,47 @@ public class DLLInsertion {
 
     Node head;
 
-    public void insertAtFirst(Node node){
-        head.setLeft(node);
-        node.setLeft(null);
-        node.setRight(head);
-        head = node;
+//    public void insertAtFirst(Node node){
+//        head.setLeft(node);
+//        node.setLeft(null);
+//        node.setRight(head);
+//        head = node;
+//    }
+
+    public void insertAtFirst(int data){
+        Node newNode = new Node(data);
+        newNode.setRight(head);
+        newNode.setLeft(null);
+        if(head!=null){
+            head.setLeft(newNode);
+        }
+        head = newNode;
     }
+
+    public void insertAtLast(int data){
+        Node newNode= new Node(data);
+        newNode.setRight(null);
+        if(head == null){
+            newNode.setLeft(null);
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp != null){
+            temp = temp.getRight();
+        }
+        temp.setRight(newNode);
+        newNode.setLeft(temp);
+    }
+
+    public void insertAtIndex(int data,int position){
+        if(position == 0){
+            insertAtFirst(data);
+            return;
+        }
+        Node newData = new Node(data);
+
+    }
+
+
 }
