c3c68660153dc53e8a4b56cccf7eb324517c573d
Charan Mareedu <charan.mareedu@reisystems.com>
2019-09-10 21:34:16 -0400

    new changes

--- src/graphs/GraphLL.java
+++ src/graphs/GraphLL.java
@@ -1,4 +1,26 @@
 package graphs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class GraphLL {
+
+    static LinkedList<Integer>[] vertices;
+
+    GraphLL(int v){
+        vertices = new LinkedList[v];
+        for(int i=0;i<v;i++){
+            vertices[i] = new LinkedList<>();
+        }
+    }
+
+    static void addEdge(int i,int j){
+        vertices[i].add(j);
+        vertices[j].add(i);
+    }
+
+    public static void main(String[] args) {
+        GraphLL graphLL = new GraphLL(5);
+
+    }
 }
